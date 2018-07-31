5a443aa5de42a67e6a25970478a7821f4cfd93f4
Code <Code@batc.local>
2018-07-31 08:32:15 -0600

    first commit

---
+++ Javascript/title_screen.js
@@ -0,0 +1,7 @@
+// document.getElementById("play").addEventListener("click", myFunction);
+// var title = document.getElementById("title_screen")
+
+// function myFunction() {
+//         title_screen.className = "hide";
+//         help.className = "hide";
+// }
