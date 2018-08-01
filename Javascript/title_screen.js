45054ed41e10abba04e0f7559b7b26dc46f81706
Code <Code@batc.local>
2018-08-01 08:22:35 -0600

    Portugal folders

--- Javascript/title_screen.js
+++
@@ -1,7 +0,0 @@
-// document.getElementById("play").addEventListener("click", myFunction);
-// var title = document.getElementById("title_screen")
-
-// function myFunction() {
-//         title_screen.className = "hide";
-//         help.className = "hide";
-// }
