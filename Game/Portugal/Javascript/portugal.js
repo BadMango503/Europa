325101284d2d58e40b1498f6ff3f07e740eb3b19
Code <Code@batc.local>
2018-08-01 10:36:11 -0600

    brocken

---
+++ Game/Portugal/Javascript/portugal.js
@@ -0,0 +1,6 @@
+document.getElementById("cc").addEventListener("click", myFunction);
+
+//var title = document.getElementById("intro")
+ function myFunction() {
+        intro.className = "hide";
+}
